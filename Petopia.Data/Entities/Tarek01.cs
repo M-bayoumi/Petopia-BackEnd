2e170885d5e8c805d86da657183f11dc55e356c9
M-bayoumi <mbayoumi151@gmail.com>
2024-01-06 22:41:59 +0200

    delete tarek 01

--- Petopia.Data/Entities/Tarek01.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Petopia.Data.Entities
-{
-    public class Tarek01
-    {
-        public int MyProperty { get; set; }
-        public int MyProperty1 { get; set; }
-        public int MyProperty2 { get; set; }
-    }
-}
