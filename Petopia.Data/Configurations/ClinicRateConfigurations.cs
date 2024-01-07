24f940e3c8155f9d3eb62cc662c696e17b271e8d
Abdllaibrahim <abdllaibrahim22@gmail.com>
2024-01-07 22:27:49 +0200

    A1

---
+++ Petopia.Data/Configurations/ClinicRateConfigurations.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Petopia.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petopia.Data.Configurations
+{
+    public class ClinicRateConfigurations : IEntityTypeConfiguration<ClincRate>
+    {
+        public void Configure(EntityTypeBuilder<ClincRate> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Comment)
+                .HasColumnType("varchar(max)")
+                .IsRequired();
+
+            builder.Property(x => x.Rate)
+               .HasColumnType("decimal")
+               .IsRequired();
+
+            builder.Property(x => x.UserId)
+               .HasColumnType("int")
+               .IsRequired();
+
+            builder.Property(x => x.ClinicId)
+               .HasColumnType("int")
+               .IsRequired();
+
+            builder.HasOne(x => x.User)
+                .WithMany(x => x.ClincRates)
+                .HasForeignKey(x => x.UserId)
+                .IsRequired();
+
+            builder.HasOne(x => x.Clinics)
+                .WithMany(x => x.ClincRates)
+                .HasForeignKey(x => x.ClinicId)
+                .IsRequired();
+
+            builder.ToTable("ClincRate");
+        }
+    }
+}
---
+++ Petopia.Data/Entities/Worktime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petopia.Data.Entities
+{
+    public class Worktime
+    {
+        public int Id { get; set; }
+
+        public int ClincId { get; set; }
+
+        public string Day { get; set; }
+
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public Clinics Clinics { get; set; } = null!;
+
+        public Booking Bookings { get; set; } = null!;
+    }
+}
