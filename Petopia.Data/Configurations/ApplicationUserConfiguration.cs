9c2b238b1239ef9f4f970143dbd66e434840c754
M-bayoumi <mbayoumi151@gmail.com>
2024-01-07 21:38:44 +0200

    tarek00

--- Petopia.Data/Configurations/ApplicationUserConfiguration.cs
+++ Petopia.Data/Configurations/ApplicationUserConfiguration.cs
@@ -11,15 +11,54 @@ namespace Petopia.Data.Configurations
             builder.HasKey(x => x.Id);
 
             builder.Property(x => x.FirstName)
-               .HasColumnType("varchar")
-               .HasMaxLength(10)
+               .HasColumnType("varchar(max)")
                .IsRequired();
 
             builder.Property(x => x.LastName)
-               .HasColumnType("varchar")
-               .HasMaxLength(10)
-
+               .HasColumnType("varchar(max)")
                .IsRequired();
+
+            builder.Property(x => x.BirthDate)
+              .HasColumnType("dateTime")
+              .IsRequired();
+
+            builder.Property(x => x.Gender)
+               .HasColumnType("int")
+               .IsRequired();
+
+            builder.Property(x => x.blocked)
+              .HasColumnType("bit")
+              .IsRequired();
+
+            builder.Property(x => x.Street)
+               .HasColumnType("varchar(max)")
+               .IsRequired();
+        }
+    }
+    public class AreaConfiguration : IEntityTypeConfiguration<Area>
+    {
+        public void Configure(EntityTypeBuilder<Area> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+               .HasColumnType("varchar(max)")
+               .IsRequired();
+        }
+    }
+    public class BlockConfiguration : IEntityTypeConfiguration<Block>
+    {
+        public void Configure(EntityTypeBuilder<Block> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.BlockedUserId)
+               .HasColumnType("varchar(max)")
+               .IsRequired();
+
+            builder.Property(x => x.Date)
+              .HasColumnType("dateTime")
+              .IsRequired();
         }
     }
 }
--- Petopia.Data/Entities/ApplicationUser.cs
+++ Petopia.Data/Entities/ApplicationUser.cs
@@ -7,12 +7,9 @@ namespace Petopia.Data.Entities
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public DateOnly BirthDate { get; set; } = new DateOnly();
         public GenderEnum Gender { get; set; }
-        public DateOnly BirthDate { get; set; }
-        public byte[] ProfileImage { get; set; } = new byte[0];
-        public byte[] Cover { get; set; } = new byte[0];
         public bool blocked { get; set; } = false;
-
 
         public int CountryId { get; set; }
         public Country Country { get; set; } = null!;
@@ -22,15 +19,17 @@ namespace Petopia.Data.Entities
         public Area Area { get; set; } = null!;
         public string Street { get; set; } = string.Empty;
 
+        public virtual ICollection<ProfileImage> ProfileImages { get; set; } = new HashSet<ProfileImage>();
+        public virtual ICollection<CoverImage> CoverImages { get; set; } = new HashSet<CoverImage>();
         public virtual ICollection<Friend> Friends { get; set; } = new HashSet<Friend>();
         public virtual ICollection<Follower> Followers { get; set; } = new HashSet<Follower>();
         public virtual ICollection<Following> Followings { get; set; } = new HashSet<Following>();
-        public virtual ICollection<Photo> Photos { get; set; } = new HashSet<Photo>();
         public virtual ICollection<Block> BlockList { get; set; } = new HashSet<Block>();
         public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
         public virtual ICollection<Reaction> Reactions { get; set; } = new HashSet<Reaction>();
         public virtual ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
         public virtual ICollection<Share> Shares { get; set; } = new HashSet<Share>();
+        public virtual ICollection<Photo> Photos { get; set; } = new HashSet<Photo>();
 
     }
 }
