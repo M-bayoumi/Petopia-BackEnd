ebe433da44e830d20486ce32e2b3f5e82c440991
Mahdeei <mohamedmahdeei6@gmail.com>
2024-01-07 20:59:56 +0200

    ragab team 1

---
+++ Petopia.Data/Entities/ShelterRate.cs
@@ -0,0 +1,16 @@
+﻿namespace Petopia.Data.Entities
+{
+    public class ShelterRate
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; } = null!;
+        public string Rate { get; set; } = null!;
+        public int ShelterId { get; set; }
+        public string UserId { get; set; } = null!;
+
+
+
+        public virtual ICollection<ApplicationUser> Users { get; set; } = null!;
+        public Shelter shelter { get; set; } = null!;
+    }
+}
